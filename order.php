525b8feb6566ca9a72f115eda19c768c43929b26
Leszek Sadkowski <36760498+LeszekSadkowski@users.noreply.github.com>
2019-05-06 12:37:53 +0200

    konfigurator

--- order.php
+++ order.php
@@ -1,7 +1,9 @@
 <?php 
 include ('includes/header.php');
 include ("includes/nav.php");?> 
-
+<iframe src="https://konfigurator.zlotyskorpion.com/detal/index.php?id=39181d09209cd4a287e1cacf5e9795c4" 
+style="margin: 0;padding: 0; border: none; width: 1000px; height: 600px;" >
+</iframe>
 
 
 <script>
